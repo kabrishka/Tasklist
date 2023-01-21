5b25300f618add74258c7a40ed09b2e0c922e44c
Karina <99499702+kabrishka@users.noreply.github.com>
2023-01-21 06:14:23 +0300

    Delete Topics/Destructuring declarations directory

--- Topics/Destructuring declarations/Fix a bug/src/Main.kt
+++
@@ -1,8 +0,0 @@
-data class Article(val name: String, val pages: Int, val author: String)
-
-fun getArticleByName(articles: MutableList<Article>, name: String): Article? {
-    var index = 0
-    articles.forEach { (title, _, _) -> if (title == name) return articles[index] else index++}
-    
-    return null
-}
